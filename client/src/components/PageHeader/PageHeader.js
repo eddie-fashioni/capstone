e55d9846a3cfacb7e719e05b4b0e54f8593e5328
eddie fashioni <fashioni.eddie@gmail.com>
2022-06-22 20:38:19 -0400

    Nav bar styling almost complete with nav links included

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -1,5 +1,5 @@
 import "./PageHeader.scss";
-import { Link } from "react-router-dom";
+import { Link , NavLink } from "react-router-dom";
 import logo from "../../assets/images/take5-black.png";
 
 const PageHeader = () => {
@@ -9,6 +9,11 @@ const PageHeader = () => {
           <Link to="/" className="nav-logo__link">
             <img className="nav-logo__image" src={logo} alt="Brainflix Logo" />
           </Link>
+        </div>
+        <div className="nav-routes">
+          <NavLink to="/" className="nav-routes__home" activeClassName="active">Home</NavLink>
+          <NavLink to="/feedback" className="nav-routes__score" activeClassName="active">Score</NavLink>
+          <NavLink to="/dashboard" className="nav-routes__dash" activeClassName="active">Dash</NavLink>
         </div>
       </nav>
   )
