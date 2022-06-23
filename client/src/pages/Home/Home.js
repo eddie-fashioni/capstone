2c39f4f8daadca58644d9bee8eb32c0b0beaa7aa
eddie fashioni <fashioni.eddie@gmail.com>
2022-06-22 21:15:05 -0400

    Mobile styling complete with user pic added

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import "./Home.scss";
+
+class Home extends Component {
+  state = {
+
+  };
+
+componentDidMount() {
+
+}
+
+render() {
+  return
+}
+
+}
+
+export default Home
