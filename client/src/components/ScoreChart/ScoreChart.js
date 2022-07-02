0a3f3c5e9208d6e005e6ffd63e929d9afac2251e
eddie fashioni <fashioni.eddie@gmail.com>
2022-07-02 19:22:37 -0400

    Updated styling of categories

--- client/src/components/ScoreChart/ScoreChart.js
+++ client/src/components/ScoreChart/ScoreChart.js
@@ -1,3 +1,4 @@
+import "./ScoreChart.scss";
 import { useEffect, useState } from "react";
 import {
   Chart as ChartJS,
@@ -42,23 +43,43 @@ function ScoreChart() {
       datasets: [
         {
           label: "Sleep",
-          data: [3, 2, 3, 4, 5, 4, 5]
+          borderWidth: 1.5,
+          borderColor: "#00aedb",
+          backgroundColor: "#00aedb",
+          data: [3, 2, 3, 4, 5, 4, 5],
+          spanGaps: false
         },
         {
           label: "Fatigue",
-          data: [2, 1, 3, 3, 3, 4, 4]
+          borderWidth: 1.5,
+          borderColor: "#8ec127",
+          backgroundColor: "#8ec127",
+          data: [2, 1, 3, 3, 3, 4, 4],
+          spanGaps: false
         },
         {
           label: "Stress",
-          data: [4, 2, 3, 5, 5, 3, 2]
+          borderWidth: 1.5,
+          borderColor: "#a200ff",
+          backgroundColor: "#a200ff",
+          data: [4, 2, 3, 5, 5, 3, 2],
+          spanGaps: false
         },
         {
           label: "Soreness",
-          data: [1, 1, 3, 4, 3, 4, 2]
+          borderWidth: 1.5,
+          borderColor: "#d41243",
+          backgroundColor: "#d41243",
+          data: [1, 1, 3, 4, 3, 4, 2],
+          spanGaps: false
         },
         {
           label: "Motivation",
-          data: [4, 4, 3, 2, 5, 4, 3]
+          borderWidth: 1.5,
+          borderColor: "#f47835",
+          backgroundColor: "#f47835",
+          data: [4, 4, 3, 2, 5, 4, 3],
+          spanGaps: false
         }
       ],
     });
@@ -77,9 +98,9 @@ function ScoreChart() {
 }, [])
 
   return (
-    <>
+    <div className="category-chart">
       <Line options={chartOptions} data={chartData}/>
-    </>
+    </div>
   );
 }
 
--- client/src/pages/Dash/Dash.js
+++ client/src/pages/Dash/Dash.js
@@ -6,13 +6,14 @@ function Dash() {
   return (
     <>
       <div className="summary">
-        <h2 className="summary-header">Here is how you're looking this week Eddie!</h2>
+        <div className="summary-greeting">
+        <h2 className="summary-header">Here is how your looking this week Eddie!</h2>
         <p className="summary-message">
           Check out your weekly roundup in the chart below. 
-          <br></br>You will see our 5 categories listed from Monday-Sunday.
         </p>
-      </div>
+        </div>
       <ScoreChart/>
+      </div>
     </>
   );
 }
